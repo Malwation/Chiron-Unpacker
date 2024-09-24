cfd175f4e71017aa0e295648b5074ad929b40b2f
utkucorbaci <utku.corbaci@malwation.com>
2024-09-24 16:10:16 +0300

    First push

---
+++ Chiron-Unpacker/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chiron_Unpacker
+{
+    public static class Logger
+    {
+        public static void Info(string text)
+        {
+            Console.WriteLine($"[+] {text}");
+        }
+    }
+}
---
+++ Chiron-Unpacker/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Chiron_Unpacker
+{
+    public class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "TODO")]
+        public string File { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "")]
+        public string Output { get; set; }
+    }
+}
